fc1a2a5955be04cb9e8eba38a17677be4d6bbf58
DANIELLE ALBA <danib.sgh@gmail.com>
2019-01-31 20:52:10 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var chocolateBars = ['snicker', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(arr, elem){
+ return [elem, ...arr]
+
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, elem){
+  arr.unshift(elem)
+  return arr
+}
+
+function addElementToEndOfArray(arr, elem){
+  return [...arr, elem]
+}
+
+function destructivelyAddElementToEndOfArray(arr, elem){
+  arr.push(elem)
+  return arr
+}
